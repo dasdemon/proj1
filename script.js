7990196eaa26817343e6e51ae0d9b5fa74f1707c
dasdemon <dasdemon4ik@gmail.com>
2019-09-11 16:35:06 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,110 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+/*
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+
+let persone = { 
+
+    name: "John",
+    age: 25,
+    isMerrien: false
+};
+console.log(persone.name); 
+console.log(persone["name"]);
+*/
+//let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+//console.log(arr[0]);
+
+// alert("Hello World!"); // всплывающее окно с кнопкой ок
+// confirm("Are you here?") // всплывающее окно с кнопками ок и отмена
+
+//let answer = confirm("Are you here?");
+//console.log(answer); // возвращает булевые значение true или false
+
+//let answer = prompt("18?", "Да"); //записыват введенное значение в поле
+//console.log(answer);
+
+//let answer = prompt("18?", "");
+//console.log(typeof(answer)); //определяет тип введенных данных (string)
+
+//let answer = prompt("18?", "");
+//console.log(typeof(arr)); //определяет тип введенных данных (object)
+
+
+
+//less 6 Операторы
+
+// let answer = +prompt("18?", "");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " = object");
+
+// console.log(4 + " = object");
+/*
+let incr = 10,
+    decr = 10;
+console.log(incr++); //уменьшение на 1
+console.log(decr--); //увеличение на 1
+
+console.log(5%2); // вывод остатка деления первого числа на второе
+
+
+console.log("2" == 2);  // == сравнение по значениям
+console.log("2" === 2); // === сравнение по типам данных
+*/
+/*let isCheckerd = true,
+    isClose = false;
+
+console.log(isCheckerd && isClose); //&& (и) сравнивает два значения на true
+*/
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);  //|| (или) возвращает frue если хотябы 1 из элементов true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* less 1
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(second);
+*/
